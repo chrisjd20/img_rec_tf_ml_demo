b8644a978d583f0d4804a9a476e481d86646e583
chrisjd20 <chrisjd20@gmail.com>
2019-11-27 11:19:16 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# img_rec_tf_ml_demo
-Image Recognition Using TensorFlow Machine Learning Demo Using Apples and Bananas
+# Image Recognition Using TensorFlow Machine Learning Demo
+
+### Simple Demonstration to Train a Machine Learning Model to recognize Apples from Bananas
+
+### Installation (Tested on Ubuntu 18.04):
+
+```
+git clone https://github.com/chrisjd20/img_rec_tf_ml_demo.git
+cd img_rec_tf_ml_demo
+sudo apt install python3 python3-pip -y
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install --upgrade setuptools
+sudo python3 -m pip install --upgrade tensorflow==1.15
+```
+
+This will create two files we will use at:
+
+1. `/tmp/retrain_tmp/output_graph.pb`     - Trained Machine Learning Model
+2. `/tmp/retrain_tmp/output_labels.txt`   - Labels for Images
+
+### Training a TensowFlow ML Model Based on Images in `./training_images/` Folder:
+
+```
+python3 retrain.py --image_dir ./training_images/
+```
+<sub><sup>`retrain.py` is a slightly modified version of https://raw.githubusercontent.com/tensorflow/hub/master/examples/image_retraining/retrain.py</sup></sub>
+
+### Predicting Images in the `unknown_images` folder based on our trained Model:
+```
+chmod 755 predict_images_using_trained_model.py
+./predict_images_using_trained_model.py
+```
+
+### Remove Temp Cache Files Between Different Retrains
+
+```
+rm -rf /tmp/retrain_tmp /tmp/tfhub_modules
+```
---
+++ predict_images_using_trained_model.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+# Image Recognition Using Tensorflow Exmaple.
+# Code based on example at:
+# https://raw.githubusercontent.com/tensorflow/tensorflow/master/tensorflow/examples/label_image/label_image.py
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.ERROR)
+import numpy as np
+import threading
+import queue
+import time
+import sys
+
+# sudo apt install python3-pip
+# sudo python3 -m pip install --upgrade pip
+# sudo python3 -m pip install --upgrade setuptools
+# sudo python3 -m pip install --upgrade tensorflow==1.15
+
+def load_labels(label_file):
+    label = []
+    proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+    for l in proto_as_ascii_lines:
+        label.append(l.rstrip())
+    return label
+
+def predict_image(q, sess, graph, image_bytes, img_full_path, labels, input_operation, output_operation):
+    image = read_tensor_from_image_bytes(image_bytes)
+    results = sess.run(output_operation.outputs[0], {
+        input_operation.outputs[0]: image
+    })
+    results = np.squeeze(results)
+    prediction = results.argsort()[-5:][::-1][0]
+    q.put( {'img_full_path':img_full_path, 'prediction':labels[prediction].title(), 'percent':results[prediction]} )
+
+def load_graph(model_file):
+    graph = tf.Graph()
+    graph_def = tf.GraphDef()
+    with open(model_file, "rb") as f:
+        graph_def.ParseFromString(f.read())
+    with graph.as_default():
+        tf.import_graph_def(graph_def)
+    return graph
+
+def read_tensor_from_image_bytes(imagebytes, input_height=299, input_width=299, input_mean=0, input_std=255):
+    image_reader = tf.image.decode_png( imagebytes, channels=3, name="png_reader")
+    float_caster = tf.cast(image_reader, tf.float32)
+    dims_expander = tf.expand_dims(float_caster, 0)
+    resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+    normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+    sess = tf.compat.v1.Session()
+    result = sess.run(normalized)
+    return result
+
+def main():
+    # Loading the Trained Machine Learning Model created from running retrain.py on the training_images directory
+    graph = load_graph('/tmp/retrain_tmp/output_graph.pb')
+    labels = load_labels("/tmp/retrain_tmp/output_labels.txt")
+
+    # Load up our session
+    input_operation = graph.get_operation_by_name("import/Placeholder")
+    output_operation = graph.get_operation_by_name("import/final_result")
+    sess = tf.compat.v1.Session(graph=graph)
+
+    # Can use queues and threading to spead up the processing
+    q = queue.Queue()
+    unknown_images_dir = 'unknown_images'
+    unknown_images = os.listdir('unknown_images')
+    for image in unknown_images:
+        img_full_path = '{}/{}'.format(unknown_images_dir, image)
+        
+        print('Processing Image {}'.format(img_full_path))
+        # We don't want to process too many images at once. 10 threads max
+        while len(threading.enumerate()) > 10:
+            time.sleep(0.0001)
+
+        image_bytes = open(img_full_path,'rb').read()
+        threading.Thread(target=predict_image, args=(q, sess, graph, image_bytes, img_full_path, labels, input_operation, output_operation)).start()
+    
+    print('Waiting For Threads to Finish...')
+    while q.qsize() < len(unknown_images):
+        time.sleep(0.001)
+    
+    prediction_results = [q.get() for x in range(q.qsize())]
+    for prediction in prediction_results:
+        print('TensorFlow Predicted {img_full_path} is a {prediction} with {percent:.2%} Accuracy'.format(**prediction))
+
+if __name__ == "__main__":
+    main()
